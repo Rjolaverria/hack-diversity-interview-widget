f5320b1e837e1c6975a75192643c0727177a0c6d
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2021-04-06 13:52:32 -0400

    Add scroll to last message on update

--- src/components/SingleConversation/index.js
+++ src/components/SingleConversation/index.js
@@ -1,83 +1,87 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, createRef } from 'react';
 import { connect } from 'react-redux';
 
 import Message from '../Message';
 import { sendMessage } from '../../modules/message/actions';
 import './style.css';
 
-
 class SingleConversation extends PureComponent {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      messageInput: '',
-    };
-  }
-
-  onChangeInput = (e) => {
-    this.setState({
-      messageInput: e.target.value,
-    });
-  }
-
-  maybeSubmit = (e) => {
-    if (e.keyCode === 13) {
-      e.preventDefault();
-      this.props.dispatcher.sendMessage(this.state.messageInput, this.props.conversationId);
-      this.setState({
-        messageInput: '',
-      });
+        this.state = {
+            messageInput: '',
+        };
+        this.endRef = createRef();
     }
-  }
 
-  render() {
-    const {
-      messages,
-    } = this.props;
+    componentDidUpdate() {
+        this.endRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
 
-    const {
-      messageInput,
-    } = this.state;
+    onChangeInput = (e) => {
+        this.setState({
+            messageInput: e.target.value,
+        });
+    };
 
-    return (
-        <div className='drift-sidebar-single-conversation--container'>
-            <div className='drift-sidebar-single-conversation-body'>
-                {messages.map((message) => (
-                    <Message
-                        key={message.id}
-                        message={message}
-                        messages={messages}
+    maybeSubmit = (e) => {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+            this.props.dispatcher.sendMessage(
+                this.state.messageInput,
+                this.props.conversationId
+            );
+            this.setState({
+                messageInput: '',
+            });
+        }
+    };
+
+    render() {
+        const { messages } = this.props;
+
+        const { messageInput } = this.state;
+
+        return (
+            <div className='drift-sidebar-single-conversation--container'>
+                <div className='drift-sidebar-single-conversation-body'>
+                    {messages.map((message) => (
+                        <Message
+                            key={message.id}
+                            message={message}
+                            messages={messages}
+                        />
+                    ))}
+                      <div ref={this.endRef} />
+                </div>
+                {/* Should be moved to own component to prevent unnecessary re-renders */}
+                <div className='drift-sidebar-single-conversation-input'>
+                    <input
+                        placeholder='Type and press enter to send'
+                        value={messageInput}
+                        onChange={this.onChangeInput}
+                        onKeyDown={this.maybeSubmit}
                     />
-                ))}
+                </div>
             </div>
-            {/* Should be moved to own component to prevent unnecessary re-renders */}
-            <div className='drift-sidebar-single-conversation-input'>
-                <input
-                    placeholder='Type and press enter to send'
-                    value={messageInput}
-                    onChange={this.onChangeInput}
-                    onKeyDown={this.maybeSubmit}
-                />
-            </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
-const mapStateToProps = state => {
-  const conversationId = state.conversation.selectedConversation;
-  return {
-    messages: state.message.byConversationId[conversationId] || [],
-    conversationId,
-  }
-}
+const mapStateToProps = (state) => {
+    const conversationId = state.conversation.selectedConversation;
+    return {
+        messages: state.message.byConversationId[conversationId] || [],
+        conversationId,
+    };
+};
 
-const mapDispatchToProps = dispatch => ({
-  dispatcher: {
-    sendMessage: (messageBody, conversationId) => dispatch(sendMessage({ body: messageBody, conversationId, })),
-  }
-})
-
+const mapDispatchToProps = (dispatch) => ({
+    dispatcher: {
+        sendMessage: (messageBody, conversationId) =>
+            dispatch(sendMessage({ body: messageBody, conversationId })),
+    },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(SingleConversation);
